8f406846d0f68bbe155457a05ed0e06fc06fea30
pWhitS <patrickdwhitsell@gmail.com>
2017-02-10 21:12:38 -0500

    Production readiness

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # CheckersAI
 A Checkers playing Artificial Intelligence, utilizing multiple game playing AI techniques.
 
+
+## Quick Start
+Download this repository:
+```
+git clone https://github.com/pWhitS/CheckersAI.git
+```
+Or download the ZIP file above and extract it.
+
+
+
+
+
+
 ## How does it work?
 This repository actually contains [eventually...] two AI checkers players. At a basic level, the first AI player uses an Alpha-Beta search tree to select moves by looking several steps ahead in the game.  The second AI player [will] uses a Monte-Carlo search tree to pseudorandomly simulate possible game play, and pick the next move based on these simulations. 
 
--- run.py
+++ run.py
@@ -14,7 +14,7 @@ from MonteCarloAI import *
 # timeout: The maximum time to search the game tree. Returns best answer on timeout.
 ################
 
-#BASIC AI PLAYERS
+# BASIC AI PLAYERS
 # Anything more than 3 is risky (very slow) in the mid game...
 basic_player = lambda board: alpha_beta(board, depth=3, eval_fn=basic_evaluate)
 
@@ -23,7 +23,7 @@ progressive_player = lambda board: progressive_deepener(board,
 													  search_fn=alpha_beta,
 													  eval_fn=basic_eval_memoized,
 													  get_next_moves_fn=get_ordered_moves_helper,
-													  timeout=15)
+													  timeout=10)
 
 # Longer times are necessary to get good sampling
 # Current implimentation is slow and not very good
@@ -33,6 +33,22 @@ mcts_player = lambda board: monte_carlo_search(board, get_all_next_moves, timeou
 # ADVANCED AI PLAYERS
 # Combinations of techniques. Very experimental.
 
+# Nothing here yet...
 
-run_game(progressive_player, human_player)
+
+## PLAY AGAINST THE AI PLAYERS
+# run_game(human_player, basic_player)
+# run_game(basic_player, human_player)
+
+run_game(human_player, progressive_player)
+# run_game(progressive_player, human_player)
+
+
+## PLAY AI AGAINST THEMSELVES
+# run_game(progressive_player, basic_player)
+# run_game(basic_player, progressive_player)
+# run_game(progressive_player, progressive_player)
+
+
+
 
